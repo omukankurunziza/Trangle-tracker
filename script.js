0bf28714929b1c5e1abceb567efbf32b2a7e1cb0
mukankunziza opportunee <nshutioppo@yahoo.fr>
2018-12-14 15:13:04 +0200

    Add variable side

---
+++ script.js
@@ -0,0 +1,6 @@
+function getResult(){
+    var side1 = parseFloat(document.getElementById("side1").value);
+    var side2 = parseFloat(document.getElementById("side2").value);
+    var side3 = parseFloat(document.getElementById("side3").value);
+    
+}
