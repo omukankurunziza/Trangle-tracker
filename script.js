a401e1f6288f3bb1912164b30b2edb77e7f4ac33
mukankunziza opportunee <nshutioppo@yahoo.fr>
2018-12-17 15:35:08 +0200

    change length of side

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+var sideOfTriangle = ["side1","side2","side3"];
 function getResult(){
     var side1 = parseFloat(document.getElementById('side1').value);
     var side2 = parseFloat(document.getElementById('side2').value);
     var side3 = parseFloat(document.getElementById('side3').value);
-  if (side1 === side2 && side2 === side3){
+  if (side1 === side2 && side1 === side3){
       alert("This is an Equirateral triangle");
       window.location.href="equirateral.html";
   }  
